3de439c9fea58f181a8c6d42a8d4907f3025e0dc
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 11:42:24 +0200

    feature: implement default metadata in root layout

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -1,10 +1,22 @@
 import { Box } from '@chakra-ui/react';
+import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
 import React, { PropsWithChildren } from 'react';
 
 interface RootLayoutProps extends PropsWithChildren {}
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  return <Box>{children}</Box>;
+  const { t } = useTranslation('common');
+  return (
+    <Box bg="tomato">
+      <Head>
+        <title>t('common:meta.title')</title>
+        <meta name="description" content={t('common:meta.description')} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </Box>
+  );
 };
 
 export default RootLayout;
