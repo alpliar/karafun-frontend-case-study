4262f8b4c3c3039c862084cd904d94c95b35b5fb
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 12:55:57 +0200

    feature: apply header to root layout

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -1,10 +1,16 @@
 import { Box } from '@chakra-ui/react';
 import React, { PropsWithChildren } from 'react';
+import Header from '../Header';
 
 // interface RootLayoutProps extends PropsWithChildren {}
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Box bg="tomato">{children}</Box>;
+  return (
+    <Box bg="tomato">
+      <Header />
+      {children}
+    </Box>
+  );
 };
 
 export default RootLayout;
