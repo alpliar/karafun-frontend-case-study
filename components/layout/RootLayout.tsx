82307744115000aab3c1b07240dfa80617739793
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 11:50:26 +0200

    fix: layout component shouldnt use translations

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -6,17 +6,7 @@ import React, { PropsWithChildren } from 'react';
 // interface RootLayoutProps extends PropsWithChildren {}
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  const { t } = useTranslation('common');
-  return (
-    <Box bg="tomato">
-      <Head>
-        <title>t('common:meta.title')</title>
-        <meta name="description" content={t('common:meta.description')} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {children}
-    </Box>
-  );
+  return <Box bg="tomato">{children}</Box>;
 };
 
 export default RootLayout;
