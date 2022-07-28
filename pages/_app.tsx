854c9fd95db6312f81aa12724417ea07522a9db1
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 10:43:37 +0200

    feature: use i18n HOC / provider in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,4 +12,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
