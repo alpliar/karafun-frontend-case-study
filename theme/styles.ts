5e06a4ea9e090ac7c4715bfdddadf1d88ac4a5bf
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 16:16:56 +0200

    style: update default style

--- theme/styles.ts
+++ theme/styles.ts
@@ -1,11 +1,23 @@
 import { mode, Styles } from '@chakra-ui/theme-tools';
 
 const styles: Styles = {
-    global: (props) => ({
-        body: {
-            bg: mode('white', 'gray.800')(props)
-        }
-    })
+  global: (props) => {
+    const defaultSpacing = { base: 2, sm: 8, md: 12 };
+    const biggerSpacing = { base: 4, sm: 12, md: 16 };
+
+    return {
+      body: {
+        bg: mode('#E9ECFE', '#351791')(props),
+        color: mode('midnightblue', 'white')(props),
+      },
+      main: {
+        bg: mode('white', 'black')(props),
+        padding: biggerSpacing,
+        margin: defaultSpacing,
+        rounded: '2xl',
+      },
+    };
+  },
 };
 
 export default styles;
