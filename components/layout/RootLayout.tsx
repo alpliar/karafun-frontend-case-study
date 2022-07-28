94021878c49d33138a2793286e4a45085ff42feb
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-28 11:45:56 +0200

    fix: @typescript-eslint/no-empty-interface

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -3,9 +3,9 @@ import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import React, { PropsWithChildren } from 'react';
 
-interface RootLayoutProps extends PropsWithChildren {}
+// interface RootLayoutProps extends PropsWithChildren {}
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   const { t } = useTranslation('common');
   return (
     <Box bg="tomato">
