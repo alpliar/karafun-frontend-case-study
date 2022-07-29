7f50db8d4572a86e753d229f03b3814147ecfb36
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-29 11:53:01 +0200

    feature: use title component in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import UiLink from '../components/UiLink';
+import UiTitle from '../components/UiTitle';
 
 const Home: NextPage = () => {
   const { t } = useTranslation(['home-page', 'common']);
@@ -18,18 +19,7 @@ const Home: NextPage = () => {
         <Text color="youpi">{t('home-page:needHelp')}</Text>
         <Text>{t('home-page:estimatedDuration', { duration: '3' })}</Text>
       </Flex>
-      <Heading
-        fontSize="5xl"
-        lineHeight={{ base: '1em', md: '1.25em' }}
-        fontWeight="bold"
-        whiteSpace="pre-wrap"
-      >
-        <Text as="span" color="#4F52FE">
-          {t('home-page:title')}
-        </Text>
-        {'\n'}
-        <Text as="span">{t('home-page:subtitle')}</Text>
-      </Heading>
+      <UiTitle title={t('home-page:title')}>{t('home-page:subtitle')}</UiTitle>
       <Divider maxW={32} borderColor="#FB6B4B" opacity={1} borderBottomWidth={4} />
       <Text whiteSpace="pre-wrap">{t('home-page:descriptionText')}</Text>
       <UiLink href="/form" asButton>
