71ea720304d23aeb37552fea8f71be3797ad2141
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-29 11:35:08 +0200

    feature: use link component for letsgo button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Button, Divider, Heading, Stack, Text } from '@chakra-ui/react';
+import { Divider, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 import type { GetStaticProps, NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
+import UiLink from '../components/UiLink';
 
 const Home: NextPage = () => {
   const { t } = useTranslation(['home-page', 'common']);
@@ -13,20 +14,27 @@ const Home: NextPage = () => {
         <meta name="description" content={t('common:meta.description')} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Text>{t('home-page:needHelp')}</Text>
-      <Text>{t('home-page:estimatedDuration', { duration: '3' })}</Text>
-      <Heading fontSize="5xl" lineHeight="1em" fontWeight="bold" whiteSpace="pre-wrap">
-        <Text as="span" color="mediumblue">
+      <Flex wrap={{ base: 'wrap', md: 'nowrap' }} justify="space-between" gap={2}>
+        <Text color="youpi">{t('home-page:needHelp')}</Text>
+        <Text>{t('home-page:estimatedDuration', { duration: '3' })}</Text>
+      </Flex>
+      <Heading
+        fontSize="5xl"
+        lineHeight={{ base: '1em', md: '1.25em' }}
+        fontWeight="bold"
+        whiteSpace="pre-wrap"
+      >
+        <Text as="span" color="#4F52FE">
           {t('home-page:title')}
         </Text>
         {'\n'}
         <Text as="span">{t('home-page:subtitle')}</Text>
       </Heading>
-      <Divider maxW={64} borderColor="darkorange" borderBottomWidth={4} />
+      <Divider maxW={32} borderColor="#FB6B4B" opacity={1} borderBottomWidth={4} />
       <Text whiteSpace="pre-wrap">{t('home-page:descriptionText')}</Text>
-      <Button colorScheme="facebook" rounded="full" maxW="3xs">
+      <UiLink href="/form" asButton>
         {t('common:letsGo')} ➜
-      </Button>
+      </UiLink>
       <Text opacity="60%">{t('home-page:usuallyCompletesIn', { minutes: 3 })}</Text>
     </Stack>
   );
