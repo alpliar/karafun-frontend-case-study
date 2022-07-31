83be6836c5e2caf6bff784c33d5072d1705ce7ea
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-31 12:48:52 +0200

    feature: add color theme toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,26 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Image,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 const Header: React.FC = ({}) => {
+  const { toggleColorMode } = useColorMode();
   const router = useRouter();
+  const ToggleIcon = useColorModeValue(MoonIcon, SunIcon);
   return (
     <Flex as="header" justify="space-between">
       <Image src="/logo.svg" alt="Karafun" width={32} height="auto" />
-      <Box>
+      <HStack>
         <NextLink href={router.asPath} passHref locale="fr">
           fr
         </NextLink>
@@ -16,7 +28,15 @@ const Header: React.FC = ({}) => {
         <NextLink href={router.asPath} passHref locale="en">
           en
         </NextLink>
-      </Box>
+        <IconButton
+          size="sm"
+          onClick={toggleColorMode}
+          aria-label="fazza"
+          icon={<Icon as={ToggleIcon} />}
+        >
+          toggle theme
+        </IconButton>
+      </HStack>
     </Flex>
   );
 };
