db62b0bf0a49e71c690bc54c64403867d0555769
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-31 12:44:42 +0200

    feature: add locale switcher

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,23 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Flex, Image } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Header: React.FC = ({}) => {
+  const router = useRouter();
   return (
-    <Box as="header">
+    <Flex as="header" justify="space-between">
       <Image src="/logo.svg" alt="Karafun" width={32} height="auto" />
-    </Box>
+      <Box>
+        <NextLink href={router.asPath} passHref locale="fr">
+          fr
+        </NextLink>
+        {` `}/{` `}
+        <NextLink href={router.asPath} passHref locale="en">
+          en
+        </NextLink>
+      </Box>
+    </Flex>
   );
 };
 
